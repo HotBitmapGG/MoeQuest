8e5bc3d4eda19754683e2375ad5d7d773ae4d05f
hotshotGG <100332338@qq.com>
2016-07-01 12:40:57 +0800

    HotBitmapGG
    欢迎页动画效果

--- app/src/main/java/com/hotbitmapgg/moequest/ui/activity/AppSplashActivity.java
+++ app/src/main/java/com/hotbitmapgg/moequest/ui/activity/AppSplashActivity.java
@@ -1,14 +1,23 @@
 package com.hotbitmapgg.moequest.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import com.flaviofaria.kenburnsview.KenBurnsView;
-import com.flaviofaria.kenburnsview.Transition;
 import com.hotbitmapgg.moequest.R;
-import com.hotbitmapgg.moequest.base.RxBaseActivity;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 /**
  * Created by hcc on 16/6/30 21:57
@@ -16,34 +25,52 @@ import butterknife.Bind;
  * <p/>
  * App欢迎页面
  */
-public class AppSplashActivity extends RxBaseActivity
+public class AppSplashActivity extends Activity
 {
 
     @Bind(R.id.splash_image)
-    KenBurnsView mKenBurnsView;
+    ImageView mSplashImage;
+
+    private static final int ANIMATION_TIME = 2000;
+
+    private static final float SCALE_END = 1.13F;
 
     @Override
-    public int getLayoutId()
+    protected void onCreate(Bundle savedInstanceState)
     {
 
-        return R.layout.activity_splash;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>()
+                {
+
+                    @Override
+                    public void call(Long aLong)
+                    {
+
+                        startAnim();
+                    }
+                });
     }
 
-    @Override
-    public void initViews(Bundle savedInstanceState)
+    private void startAnim()
     {
 
-        mKenBurnsView.setTransitionListener(new KenBurnsView.TransitionListener()
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mSplashImage, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mSplashImage, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(ANIMATION_TIME).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter()
         {
 
             @Override
-            public void onTransitionStart(Transition transition)
-            {
-
-            }
-
-            @Override
-            public void onTransitionEnd(Transition transition)
+            public void onAnimationEnd(Animator animation)
             {
 
                 startActivity(new Intent(AppSplashActivity.this, MainActivity.class));
@@ -51,11 +78,5 @@ public class AppSplashActivity extends RxBaseActivity
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
-    }
-
-    @Override
-    public void initToolBar()
-    {
-
     }
 }
