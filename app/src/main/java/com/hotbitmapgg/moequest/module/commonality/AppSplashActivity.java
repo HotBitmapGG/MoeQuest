745c96f7426035b9f3b4c7424835a1b3c48ee478
George Zuo <george.zuo@blackboard.com>
2016-12-03 15:43:34 +0800

    remove wrong import

--- app/src/main/java/com/hotbitmapgg/moequest/module/commonality/AppSplashActivity.java
+++ app/src/main/java/com/hotbitmapgg/moequest/module/commonality/AppSplashActivity.java
@@ -7,7 +7,6 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.AnimationSet;
 import android.widget.ImageView;
 
 import com.hotbitmapgg.moequest.R;
